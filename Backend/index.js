3bab5060890602e84b77b08344ec3f9cf73f1d77
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2024-10-18 23:39:15 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const app = require('./app');
 const cors = require('cors');
 
  const corsOptions ={
-     origin:'https://courier-devliery-web-app.vercel.app',
+     origin:'https://http://localhost:5000',
      credentials:true,            //access-control-allow-credentials:true
      optionSuccessStatus:200,
      'Access-Control-Allow-Credentials': true
